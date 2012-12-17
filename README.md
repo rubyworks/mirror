b415b3f9882e7424e37b0c37bd49144cf13407ab
7rans <transfire@gmail.com>
2012-12-17 12:29:33 -0500

    Inital commit.

---
+++ README.md
@@ -0,0 +1,46 @@
+[Homepage](http://rubyworks.github.com/real) \
+[Source Code](http://github.com/rubyworks/real) \
+[Support](http://github.com/rubyworks/real/issues)
+
+# Real
+
+There is a problem in the realm of Ruby metaprogramming --objects
+can be liers. You see, there is no guaruntee that the return value of 
+a method is the "truth". Any object can play dirty tricks.
+
+  string = "Watch this..."
+
+  def string.class
+    nil
+  end
+
+  string.class  #=> nil
+
+We are never going to know what the *real* class of that object is
+by asking it. So what can we do?
+
+I've advocated for explict meta-programming methods be added to Ruby
+for a long time. So far to no avail. Finally, I've decided I could 
+at least provide my own library that provides the functionality.
+
+  require 'real'
+
+  $real.class(string)  #=> String
+
+
+### Installation
+
+Putting the old RubyGems to work:
+
+  $ gem install real
+
+
+### Copyrights
+
+Real is copyrighted open source software.
+
+  Copyright (c) 2012 Rubyworks (BSD-2-Clause)
+
+You many redistribute or modify this work in accordance with the **BSD-2-Clause** license.
+
+See LICENSE.txt for details.
---
+++ lib/real.rb
@@ -0,0 +1,62 @@
+module Real
+  extend self
+
+  #####################
+  # Object Reflection #
+  #####################
+
+  def id(obj)
+    bind_call(Object, :object_id, *a, &b)
+  end
+
+  def class(obj)
+    bind_call(Object, :class, *a, &b)
+  end
+
+  def methods(obj)
+    bind_call(Object, :methods, *a, &b)
+  end
+
+  def public_methods(obj, *a, &b)
+    bind_call(Object, :public_methods, *a, &b)
+  end
+
+  def protected_methods(*a, &b)
+    bind_call(Object, :protected_methods, *a, &b)
+  end
+
+  def private_methods(*a, &b)
+    bind_call(Object, :private_methods, *a, &b)
+  end
+
+  ###########################
+  # Class/Module Reflection #
+  ###########################
+
+  def instance_methods(*a, &b)
+    bind_call(Module, :instanance_methods, *a, &b)
+  end
+
+  def public_instance_methods(*a, &b)
+    bind_call(Module, :public_instance_methods, *a, &b)
+  end
+
+  def protected_instance_methods(*a, &b)
+    bind_call(Module, :protected_instance_methods, *a, &b)
+  end
+
+  def private_instance_methods(*a, &b)
+    bind_call(Module, :private_instance_methods, :class, *a, &b)
+  end
+
+private
+
+  def bind_call(c, m, o, *a, &b)
+    im = c.instance_method(m) 
+    im.bind(o).call(*a, &b)    
+  end
+
+end
+
+$real = Real
+
